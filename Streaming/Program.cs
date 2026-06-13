078e7f6402cad438c0dff4aca86d20985ff7bdff
Leonardo Chavez <leonardo.chavroj@gmail.com>
2026-06-13 13:55:59 -0500

    Creación de función menú

--- Streaming/Program.cs
+++ Streaming/Program.cs
@@ -3,13 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Streaming
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            int opcion;
+
+            do
+            {
+                opcion = menu();
+
+                switch (opcion)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break; 
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        guardar_cargar();
+                        break;
+                    case 6:
+                        break;
+                }
+            }
+            while (opcion != 6);
+        }
+
+        static int menu()
+        {
+            Console.WriteLine("=== MENÚ ===\n");
+            Console.WriteLine("1) Registrar sucripciones");
+            Console.WriteLine("2) Ver suscripciones activas");
+            Console.WriteLine("3) Ver alertas de vencimiento");
+            Console.WriteLine("4) Buscar suscripción");
+            Console.WriteLine("5) Guardar/Cargar registros");   // Desde archivo
+            Console.WriteLine("6) Salir\n");
+
+            Console.Write("Ingrese una opción: ");
+            int opcion = int.Parse(Console.ReadLine());
+
+            return opcion;
+        }
+
+        static void guardar_cargar()
+        {
+            using (StreamWriter escritor = new StreamWriter("prueba.txt"))
+            {
+                escritor.WriteLine("HOLA PANCHITO CHUPA");  
+            }
         }
     }
 }
