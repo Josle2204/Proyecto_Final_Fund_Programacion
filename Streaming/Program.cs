9697933409eac496a50607fb03fce48a836ac703
Leonardo Chavez <leonardo.chavroj@gmail.com>
2026-06-15 09:10:05 -0500

    Subiendo arciho de datos, funcion cargar_archivo y mostrar clientes

--- Streaming/Program.cs
+++ Streaming/Program.cs
@@ -2,7 +2,6 @@
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
             int opcion;
@@ -14,6 +13,10 @@
                 switch (opcion)
                 {
                     case 1:
+                        Console.Clear();
+                        mostrar_clientes();
+                        Console.ReadKey();
+                        Console.Clear();
                         break;
                     case 2:
                         break;
@@ -22,37 +25,84 @@
                     case 4:
                         break;
                     case 5:
-                        guardar_cargar();
                         break;
                     case 6:
                         break;
+                    case 7:
+                        break;
                 }
             }
-            while (opcion != 6);
-        }
+            while (opcion != 7);
+        }   
 
         static int menu()
         {
+            int opcion = 0;
+            bool valido;
+
             Console.WriteLine("=== MENÚ ===\n");
-            Console.WriteLine("1) Registrar sucripciones");
-            Console.WriteLine("2) Ver suscripciones activas");
-            Console.WriteLine("3) Ver alertas de vencimiento");
-            Console.WriteLine("4) Buscar suscripción");
-            Console.WriteLine("5) Guardar/Cargar registros");   // Desde archivo
-            Console.WriteLine("6) Salir\n");
+            Console.WriteLine("1) Mostrar clientes");
+            Console.WriteLine("2) Buscar cliente");
+            Console.WriteLine("3) Registrar cuenta");
+            Console.WriteLine("4) Eliminar cuenta");
+            Console.WriteLine("5) Cuentas vencidas");
+            Console.WriteLine("6) Ganancia total");
+            Console.WriteLine("7) Salir\n");
 
             Console.Write("Ingrese una opción: ");
-            int opcion = int.Parse(Console.ReadLine()!);
+
+            do
+            {
+                valido = int.TryParse(Console.ReadLine(), out opcion);
+
+                if (opcion < 1 || opcion > 7)
+                {
+                    Console.Write("\nIngrese una opción válida: ");
+                    valido = false;
+                }
+
+            } while (!valido);
 
             return opcion;
         }
 
-        static void guardar_cargar()
+        static string[,] cargar_archivo()
         {
-            using (StreamWriter escritor = new StreamWriter("prueba.txt"))
+            using (StreamReader lector = new StreamReader("Streaming.csv"))
             {
-                escritor.WriteLine("HOLA PANCHITO CHUPA");
+                string datos = lector.ReadToEnd();
+                string[] lineas = datos.Split('\n');
+                string[,] matriz_datos = new string[100, 6];
+
+
+                for (int i = 0; i < lineas.Length; i++)
+                {
+                    string[] lineas_separadas = lineas[i].Split(';');
+
+                    for (int j = 0; j < 6; j++)
+                    {
+                        matriz_datos[i, j] = lineas_separadas[j];
+                    }
+                }
+
+                return matriz_datos;
             }
+        }
+
+        static void mostrar_clientes()
+        {
+            string[,] matriz_datos = cargar_archivo();
+
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine($"{"  TELÉFONO",-13} {"PRECIO",-8} {"VENCIMIENTO",-30} {"CUENTA",-30} {"PERFIL",-20} {"PLATAFORMA",-12}");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------");
+
+            for (int i = 0; i < matriz_datos.GetLength(0); i++)
+            {
+                Console.WriteLine($"{matriz_datos[i,0],-11} {matriz_datos[i, 1],-8:C} {matriz_datos[i, 2],-12:dd/MM/yyyy} {matriz_datos[i, 3],-48} {matriz_datos[i, 4],-20} {matriz_datos[i, 5],-12}"); ;
+            }
+
+            Console.Write("\nPresione una tecla para continuar...");
         }
     }
 }
